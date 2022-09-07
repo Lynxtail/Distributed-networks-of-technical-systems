aceed0e1d09039a3a64c65ed4234d615b76ae6dd
Lynxtail <l.gorbachev@mail.ru>
2022-09-07 22:07:25 +0400

    add tasks

---
+++ 12.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+    printf("%d", a % 10 + a / 10 % 10 + a / 100);
+    return 0;
+}
---
+++ 9.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c, d, e;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+    int numbers[] = {a, b, c, d, e};
+    int i, max_num;
+    max_num = a;
+    for (i = 1; i < 5; i++){
+        if (max_num < numbers[i]) {
+            max_num = numbers[i];
+        }
+    }
+    printf("%d", max_num);
+    return 0;
+}
