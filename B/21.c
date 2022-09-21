d4cfc48528fba215c1bbf752fec15fdba70ccbd2
Lynxtail <l.gorbachev@mail.ru>
2022-09-21 18:44:00 +0400

    fin B

--- B/21.c
+++ B/21.c
@@ -3,14 +3,12 @@
 
 
 #include <stdio.h>
-#include <ctype.h>
 
 int main(void){
-    char str_tmp[50], i;
-    scanf("%[^.]s", str_tmp);
-    for (i = 0; i < 50; i++){
-        str_tmp[i] = (char) tolower((int) str_tmp[i]);
+    char str;
+    while((str = getchar()) != '.'){
+        if (str >= 'A' && str <= 'Z') putchar(str + 'a' - 'A');
+        else putchar(str);
     }
-    printf("%s", str_tmp);
     return 0;
 }
