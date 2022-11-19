2570d1f262da7a56f28dd52f43d07a6ee507a939
Lynxtail <l.gorbachev@mail.ru>
2022-11-19 14:34:26 +0400

    finish

--- F/2.c
+++ F/2.c
@@ -3,15 +3,18 @@
 #include <stdio.h>
 
 void sort_even_odd(int n, int a[]){
-    int tmp;
-    for (int i = 0; i < n; i++)
+    int tmp, first = __INT_MAX__;
+    for (int i = 0; i < n; i++){
         if (a[i] % 2 != 0){
             tmp = a[i];
+            if (first == __INT_MAX__) first = tmp;
             for (int j = i; j < n - 1; j++) a[j] = a[j + 1];
             a[n - 1] = tmp;
             for (int l = 0; l < 20; l++) printf("%d ", a[l]); 
             printf("\n");       
         }    
+        if (a[i] == first) break;
+    }
 }
 
 int main(void)
